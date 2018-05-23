e35691cd1e71275dcae78ced04c8f836f22d97a2
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-22 20:22:07 -0500

    Moved widgets configuration to config file.
    Widgets are a configurable, customizable part of a theme.  They will change
    from project-to-project.  Therefore, they belong in a configuration file.
    
    Using this strategy, the widgets registration business logic is now reusable....
    never having to change it from project-to-project.
    
    Theme developers just go to the configuration files and customize.

---
+++ config/widgets.php
@@ -0,0 +1,40 @@
+<?php
+
+return array(
+	array(
+		'name'        => __( 'Above Header', 'pp-beans-starter' ),
+		'id'          => 'above-header-widget',
+		'beans_type'  => 'grid',
+		'description' => __( 'This is the widget area for above the header.', 'pp-beans-starter' ),
+	),
+	array(
+		'name'        => __( 'Header Right', 'pp-beans-starter' ),
+		'id'          => 'header-right-widget',
+		'beans_type'  => 'grid',
+		'description' => __( 'This is the widget area for the right of the header.', 'pp-beans-starter' ),
+	),
+	array(
+		'name'        => __( 'Before Footer', 'pp-beans-starter' ),
+		'id'          => 'before-footer',
+		'beans_type'  => 'grid',
+		'description' => __( 'This is the widget area for before the footer widgets.', 'pp-beans-starter' ),
+	),
+	array(
+		'name'        => __( 'Footer Widget 1', 'pp-beans-starter' ),
+		'id'          => 'footer-widget-1',
+		'beans_type'  => 'grid',
+		'description' => __( 'This is the widget area for footer widget 1.', 'pp-beans-starter' ),
+	),
+	array(
+		'name'        => __( 'Footer Widget 2', 'pp-beans-starter' ),
+		'id'          => 'footer-widget-2',
+		'beans_type'  => 'grid',
+		'description' => __( 'This is the widget area for footer widget 3.', 'pp-beans-starter' ),
+	),
+	array(
+		'name'        => __( 'Footer Widget 3', 'pp-beans-starter' ),
+		'id'          => 'footer-widget-3',
+		'beans_type'  => 'grid',
+		'description' => __( 'This is the widget area for footer widget 3.', 'pp-beans-starter' ),
+	),
+);
