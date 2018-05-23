02dde12eb8f7ab6ca7dfdfb775803084008561d1
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-22 19:59:41 -0500

    Made primary nav render the Beans' way.
    Beans gives us great flexibility to inject HTML where we want.  We just hook into
    the before_markup and after_markup hooks. And bam, we can render our HTML.
    
    While you can call a HTML view file here, using the `beans_open_markup_e` to
    handle the rendering gives full flexibility for plugins to modify the HTML.
    
    If you do not want to make the markup extensible, then you can directly
    load a view file.  That's actually slightly faster.

--- lib/templates/structure/menu.php
+++ lib/templates/structure/menu.php
@@ -2,33 +2,44 @@
 
 add_action( 'wp', 'pp_set_up_menu_structure' );
 /**
- * Set up the menu structure.
+ * Set up the menu structure. Removes the button class and the word 'Menu', from hamburger icon on mobile.
  *
  * @since 1.0.0
  *
  * @return void
  */
 function pp_set_up_menu_structure() {
-//remove the button class and the word 'Menu', from hamburger icon on mobile.
 	beans_remove_attribute( 'beans_primary_menu_offcanvas_button', 'class', 'uk-button' );
 	beans_remove_output( 'beans_offcanvas_menu_button' );
 	beans_remove_attribute( 'beans_widget_panel_offcanvas_menu', 'class', 'uk-panel-box' );
 }
 
-beans_remove_action( 'beans_primary_menu' );
 
-add_action( 'beans_header_after_markup', 'pp_move_nav_under_header' );
+// Relocate the primary nav to after the site's header.
+beans_replace_action_hook( 'beans_primary_menu', 'beans_header_after_markup' );
+
+beans_add_smart_action( 'beans_primary_menu_before_markup', 'pp_render_opening_primary_nav_container' );
 /**
- * Reposition the primary menu to below the header.
+ * Renders the Primary Navigation's opening container markup.
  *
  * @since 1.0.0
  *
  * @return void
  */
-function pp_move_nav_under_header() {
+function pp_render_opening_primary_nav_container() {
 	beans_open_markup_e( 'pp_primary_nav', 'div', array( 'class' => 'pp-primary-nav' ) );
+	    beans_open_markup_e( 'pp_primary_nav[container]', 'div', array( 'class' => 'uk-container uk-container-center' ) );
+}
 
-	    include _get_child_theme_directory() . '/views/primary-nav.php';
-
+beans_add_smart_action( 'beans_primary_menu_after_markup', 'pp_render_closing_primary_nav_container' );
+/**
+ * Renders the Primary Navigation's closing container markup.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function pp_render_closing_primary_nav_container() {
+	    beans_close_markup_e( 'pp_primary_nav[container]', 'div' );
 	beans_close_markup_e( 'pp_primary_nav', 'div' );
 }
--- views/primary-nav.php
+++
@@ -1,3 +0,0 @@
-<div class="uk-container uk-container-center">
-	<?php echo beans_primary_menu(); ?>
-</div>
