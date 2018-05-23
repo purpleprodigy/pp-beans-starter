04c64776abbdb2862b104da8eca1e9ada70da110
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-22 20:15:28 -0500

    Moved nested function out into global space.
    Don't nest functions instead of another function.  That's a bad practice.
    
    Why?
    
    1. It causes side effects and is really hard to test.
    2. It's harder to read and understand what's going on and why.

--- lib/templates/structure/footer.php
+++ lib/templates/structure/footer.php
@@ -11,22 +11,24 @@ add_action( 'wp', 'pp_set_up_footer_structure' );
 function pp_set_up_footer_structure() {
 	beans_wrap_markup( 'beans_footer', 'beans_footer_wrapper', 'div', array( 'class' => 'tm-footer-wrapper' ) );
 
-	//Footer Widgets
+	// Footer Widgets
 	beans_add_smart_action( 'beans_footer_wrapper_prepend_markup', 'pp_display_footer_widgets' );
-	/**
-	 * Display the 3 footer widgets.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return void
-	 */
-	function pp_display_footer_widgets() {
-		if ( ! is_active_sidebar( 'footer-widget-1' ) ) {
-			return;
-		}
+}
 
-		include_once _get_child_theme_directory() . '/views/footer-widgets.php';
+/**
+ * Display the 3 footer widgets.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function pp_display_footer_widgets() {
+
+	if ( ! is_active_sidebar( 'footer-widget-1' ) ) {
+		return;
 	}
+
+	include_once _get_child_theme_directory() . '/views/footer-widgets.php';
 }
 
 beans_modify_action_callback( 'beans_footer_content', 'beans_child_footer_content' );
